497ed0f9162404fe2710efcf33cd0e4956de89ce
pgathogo <pgathogo@gmail.com>
2022-06-21 10:57:50 +0300

    Defined method push and add main function

--- codegen.cpp
+++ codegen.cpp
@@ -6,4 +6,17 @@ class Stack{
        std::vector<T> elems;
     public:
        void push(T const& elem);
+       T pop() const;
+}
+
+template<typename T>
+void Stack<T>::push(T const& elem)
+{
+   elems.push_back(elem);
+}
+
+
+int main()
+{
+   return 0
 }
